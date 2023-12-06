ea56f31c0155407314c2e16f384a73dc1ca15f02
Ramiro Batallas <rbatallas@oshyn.com>
2023-12-06 12:17:52 -0500

    remove project

--- tools/renameFolders.ps1
+++ tools/renameFolders.ps1
@@ -2,7 +2,7 @@ Param(
         [Parameter(Position=0, Mandatory=$true)]
         [string]$OldCodeFolderName, 
         [Parameter(Position=1, Mandatory=$true)]
-        [string]$NewCodeFolderName      
+        [string]$NewCodeFolderName     
     )
 
 $excludedFoldersRegex = "\\(obj|bin|.git|.hg)\\?"
@@ -36,31 +36,72 @@ function Update-FileContent
 	}
 }
 
-try{
+
+
+function buildVS
+{
+    param
+    (
+        [parameter(Mandatory=$true)]
+        [String] $path,
+
+        [parameter(Mandatory=$false)]
+        [bool] $nuget = $true,
+        
+        [parameter(Mandatory=$false)]
+        [bool] $clean = $true
+    )
+    process
+    {
+        $msBuildExe = 'C:\Windows\Microsoft.NET\Framework\v4.0.30319\msbuild.exe'
+
+        if ($nuget) {
+            Write-Host "Restoring NuGet packages" -foregroundcolor green
+            nuget restore "$($path)"
+        }
+
+        if ($clean) {
+            Write-Host "Cleaning $($path)" -foregroundcolor green
+            & "$($msBuildExe)" "$($path)" /t:Clean /m
+        }
+
+        Write-Host "Building $($path)" -foregroundcolor green
+        & "$($msBuildExe)" "$($path)" /t:Build /m
+    }
+}
+
+
+
+function Modify-Folder-Structure{
+
+    Write-Host "Updating folder structure" -ForegroundColor Magenta
 
     $startPath = Split-Path $PSScriptRoot -Parent
     $sourceFolder = $startPath+"\src"
 
-    $includedFolders = "\\(Feature|Foundation|Project)\\?"
+    $includedFolders = "\\(Feature|Foundation|Project|Website)\\?"
     $InternalFoldersToModify = "\\($($OldCodeFolderName))\\?"
 
     $childFolders = Get-ChildItem -Directory $sourceFolder | Where-Object {$_.FullName -match $includedFolders} 
 
     $childFolders | ForEach-Object {
         $currentFolder = (Get-Item  $_.FullName )
+
+        Write-Host $currentFolder
         
         $intDirSecLvl = Get-ChildItem -Directory $currentFolder 
 
         $intDirSecLvl | ForEach-Object{
             $currentFolderSecLvl = (Get-Item  $_.FullName )
         
+            Write-Host $currentFolderSecLvl
 
             $intDirThirdLvl = Get-ChildItem -Directory $currentFolderSecLvl | Where-Object {$_.FullName -match $InternalFoldersToModify}
 
             $intDirThirdLvl | ForEach-Object {
 
                 $currentFolderThirdLvl = (Get-Item  $_.FullName )
-                
+                Write-Host $currentFolderThirdLvl
 
                 Write-Host "Folder name before rename: $($OldCodeFolderName)"  -ForegroundColor Yellow
         
@@ -83,14 +124,33 @@ try{
         
     }
 
-    Write-Host "Update Solution File" -ForegroundColor Magenta
+
+}
+
+try{
+
+    $startPath = Split-Path $PSScriptRoot -Parent
+
+    
 
     $fileExtensionsToUpdateContentRegex = "(.sln)$"
 
     $oldNamespacePrefix = $OldCodeFolderName
     $newNamespacePrefix = $NewCodeFolderName
+    #$solutionPath = $startPath+"\"+$SolutionName+".sln"
 
-     Update-FileContent -StartPath "$startPath" -OldValue $oldNamespacePrefix -NewValue $newNamespacePrefix -FileExtensionsRegex $fileExtensionsToUpdateContentRegex
+
+    Write-Host "Update folder structure" -ForegroundColor Magenta
+
+    Modify-Folder-Structure
+
+    Write-Host "Update Solution File" -ForegroundColor Magenta
+
+    Update-FileContent -StartPath "$startPath" -OldValue $oldNamespacePrefix -NewValue $newNamespacePrefix -FileExtensionsRegex $fileExtensionsToUpdateContentRegex
+
+    #Write-Host $solutionPath
+
+    #buildVS -path $solutionPath -nuget $true -clean $true
 
     Write-Host "Done" -ForegroundColor Green
 
